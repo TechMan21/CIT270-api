17c8a5ef487a863b8db76d5a45c3971de4c27489
TechMan21 <raw20004@byui.edu>
2022-01-21 13:54:41 -0700

    app_start

--- cit270-authentication-service/server.js
+++ cit270-authentication-service/server.js
@@ -1,1 +1,12 @@
 const express = require('express');
+const res = require('express/lib/response');
+
+const port = 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Hello Browser");
+});
+
+app.listen(port, ()=>{});
