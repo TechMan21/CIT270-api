3fbe05275f50380fa12504ce6988ddb4cfe6ea9c
TechMan21 <raw20004@byui.edu>
2022-01-28 14:05:00 -0700

    md5-addon

--- cit270-authentication-service/server.js
+++ cit270-authentication-service/server.js
@@ -13,9 +13,11 @@ app.get('/', (req, res) => {
     res.send("Hello Browser");
 });
 
+const md5 = require('md5');
+
 app.post('/login', (req, res) =>{
     console.log(JSON.stringify(req.body));
-    if(req.body.userName == "dillanrawlings" && req.body.password== "ThisAPI21!"){
+    if(req.body.userName == "dillanrawlings" && md5(req.body.password)== "10f2c9c97d658a44e423dc3528ffdb05"){
         res.send("Welcome!")
     }else{
         res.send("Who are you?");
