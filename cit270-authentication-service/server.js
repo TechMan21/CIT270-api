ede0ea8296788254f72de4b70943476cbc7f6274
TechMan21 <raw20004@byui.edu>
2022-03-11 13:52:45 -0700

    timer_files+login_edits

--- cit270-authentication-service/server.js
+++ cit270-authentication-service/server.js
@@ -32,6 +32,7 @@ https.createServer({
 
 app.post('/login', (req, res) =>{
     console.log(JSON.stringify(req.body));
+    console.log("Correct login made " + req.body.password)
     if(req.body.userName == "dillanrawlings" && md5(req.body.password)== "10f2c9c97d658a44e423dc3528ffdb05"){
         res.send("Welcome!")
     }else{
